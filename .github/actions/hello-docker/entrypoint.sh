e4f0251ea0a6fc9978b126b95c4b4c477702db39
vmpenya <vmpenya@gmail.com>
2024-09-23 13:51:07 +0200

    just trying again

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -2,10 +2,10 @@
 echo "::debug::Running entrypoint.sh"
 echo "::warning::This is a warning"
 echo "::error::This is an error message"
-echo "Hello $1"
-echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET"
-echo "HELLO: $HELLO"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
-echo "HELLO_TIME=$time" >> $GITHUB_ENV
+#echo "Hello $1"
+#echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET"
+#echo "HELLO: $HELLO"
+#time=$(date)
+#echo "time=$time" >> $GITHUB_OUTPUT
+#echo "HELLO_TIME=$time" >> $GITHUB_ENV
 
